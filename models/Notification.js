6e4c07ffc8ff1ac504e992cb0f44f4b11101755f
Kenneth Rofuli <rofuli.kj@gmail.com>
2025-09-04 03:10:04 -0700

    fixed indent

--- models/Notification.js
+++ models/Notification.js
@@ -1,42 +1,14 @@
 const mongoose = require('mongoose');
 
 const NotificationSchema = new mongoose.Schema({
-  recipient: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  type: {
-    type: String,
-    enum: ['note_shared', 'new_message'],
-    required: true
-  },
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  reference: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    refPath: 'referenceModel'
-  },
-  referenceModel: {
-    type: String,
-    required: true,
-    enum: ['Note', 'Message']
-  },
-  content: {
-    type: String
-  },
-  read: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  recipient: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  type: {type: String, enum: ['note_shared', 'new_message'], required: true },
+  sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  reference: {type: mongoose.Schema.Types.ObjectId, required: true, refPath: 'referenceModel' },
+  referenceModel: { type: String, required: true, enum: ['Note', 'Message'] },
+  content: {type: String },
+  read: {type: Boolean, default: false },
+  createdAt: {type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Notification', NotificationSchema);
